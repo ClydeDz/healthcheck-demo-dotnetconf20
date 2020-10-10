de9da28f017d9a6c2afadfe1a418eb504a7d713b
Clyde D'Souza <clyd94@gmail.com>
2020-10-10 18:33:18 +1300

    Add API solution
    - Sample controller that fetches 2 sales customers from database.
    - Add contents to readme about the project.

---
+++ Demo.API/Demo.API/Controllers/SalesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesController : ControllerBase
+    {
+
+        private readonly ILogger<SalesController> _logger;
+        private readonly IConfiguration _config;
+
+        public SalesController(ILogger<SalesController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> Get()
+        {
+            var connectionString = _config.GetConnectionString("dbConnectionString");
+            _logger.LogInformation($"Sales {nameof(Get)} called and connection string fetched.");
+            using (IDbConnection db = new SqlConnection(connectionString)) 
+            {
+                return db.Query<dynamic>
+                ("SELECT TOP (2) FirstName, LastName, EmailAddress FROM [SalesLT].[Customer]").ToList();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Health check demo     
+Adding health checks in a .NET Core application for .NET Conf 2020.    
+* We’ll look at setting up basic health checks for our ASP.NET Core application using the `Microsoft.AspNetCore.Diagnostics.HealthChecks` package.   
+* Next, we’ll update our health check to include a database resource that this application depends on.     
+* After that, we’ll look at filtering health checks on resources using `MapWhen` and `Predicate` and when you might require to use them.      
+
+## Clyde D'Souza  
+[Website](https://clydedsouza.net) | 
+[LinkedIn](https://www.linkedin.com/in/clydedz/) | 
+[Twitter](https://twitter.com/clydedz) | 
+[Instagram](https://instagram.com/insta_clyde) |
+[GitHub](https://github.com/clydedz) | 
+[YouTube](https://www.youtube.com/channel/UCM-rrJnAhIc8EY2Aj34Wnfg) | 
+[Medium](https://medium.com/@clydedz) | 
+
+  
