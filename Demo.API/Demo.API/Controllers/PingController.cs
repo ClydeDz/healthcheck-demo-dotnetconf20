37cd5b5dac7dbfb074ab39bba2c53ddc61655321
Clyde D'Souza <clyd94@gmail.com>
2020-10-10 20:31:12 +1300

    Add ping controller to perform health check
    If you were building a health endpoint from scratch without using any NuGet packages, this might be one way to do it.

---
+++ Demo.API/Demo.API/Controllers/PingController.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+        private readonly IConfiguration _config;
+
+        public PingController(ILogger<PingController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var connectionString = _config.GetConnectionString("dbConnectionString");
+            _logger.LogInformation($"Ping {nameof(Get)} called and connection string fetched.");
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var dbCheck = db.Query<dynamic>("SELECT 1");
+
+                if (dbCheck != null)
+                {
+                    return Ok("Healthy");
+                }
+
+                return BadRequest();
+            }
+        }
+    }
+}
